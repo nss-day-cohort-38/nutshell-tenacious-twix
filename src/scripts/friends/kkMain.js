c6bed1362ac605daaca8575d251755bf5c320c9c
Kurt Krafft <kkrafft6609@gmail.com>
2020-02-14 11:33:31 -0600

    added universal renderToDom functionality

---
+++ src/scripts/friends/kkMain.js
@@ -0,0 +1,10 @@
+import friendsEventManager from "./eventListeners.js";
+
+const kkMainManager = {
+    kkRunIt:() => {
+
+      friendsEventManager.addFriendsNavBarListener();
+
+    }
+}
+export default kkMainManager
