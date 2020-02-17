a719d7af9dcb52d0dcef20485b7cddb41f08e0cc
Andrew Green <green.andrew.b@gmail.com>
2020-02-17 10:43:50 -0600

    make sure i dont lose anything

--- src/scripts/events/eventListeners.js
+++ src/scripts/events/eventListeners.js
@@ -1,6 +1,6 @@
 import renderManager from "../renderManager.js";
-import eventAPI from "./eventsAPI.js"
-import htmlManager from "./eventsHtmlCreator.js"
+import eventAPI from "./eventsAPI.js";
+import htmlManager from "./eventsHtmlCreator.js";
 
 const eventNavButton = document.querySelector("#eventNavButton");
 const hiddenEntryId = document.querySelector("#entryId");
@@ -10,29 +10,48 @@ const dateInput = document.querySelector("#dateInput");
 const updateButton = document.querySelector("#update");
 const container = document.querySelector("#container");
 
-
-const eventsAddEventListener = (activeUserId) => {
-    eventNavButton.addEventListener("click", () => {
-       eventAPI.getEvents(activeUserId).then(events=> {
-        renderManager.renderEventsToContainer(events, htmlManager.eventsHtmlCreator)})
-    })
-}
+const eventsAddEventListener = activeUserId => {
+  eventNavButton.addEventListener("click", () => {
+    eventAPI.getEvents(activeUserId).then(events => {
+      renderManager.renderEventsToContainer(
+        events,
+        htmlManager.eventsHtmlCreator
+      );
+    });
+  });
+};
 
 const clearForm = () => {
-    hiddenEntryId.value = "";
-    nameInput.value = "";
-    locationInput.value = "";
-    dateInput.value = "";
-}
+  hiddenEntryId.value = "";
+  nameInput.value = "";
+  locationInput.value = "";
+  dateInput.value = "";
+};
 
 const updateEventListener = () => {
-    updateButton.addEventListener("click", () => {
-        container.innerHTML = "";
-        const newEvent = {
-            eventId: parseInt()
-        }
-    })
-}
+  updateButton.addEventListener("click", () => {
+    container.innerHTML = "";
+    const newEvent = {
+      userId: parseInt(activeUsers.id),
+      name: nameInput.value,
+      location: locationInput.value,
+      date: dateInput.value
+    };
+    if (hiddenEntryId.value !== "") {
+        newEvent.id = parseInt(hiddenEntryId);
+        eventAPI.updateEvent(newEvent).then(() => {
+            eventAPI.getEvents()
+            .then(renderManager.renderEventsToContainer)
+            .then(clearForm);
+        });
+    } else {
+        eventAPI.saveEvent(newEvent).then(() => {
+            eventAPI.getEvents()
+            .then(renderManager.renderEventsToContainer)
+            .then(clearForm);
+        });
+    }
+  });
+};
 
 export default eventsAddEventListener;
-
--- src/scripts/events/eventsAPI.js
+++ src/scripts/events/eventsAPI.js
@@ -1,9 +1,47 @@
 const eventAPI = {
-    getEvents(activeUserId) {
-        return fetch(`http://localhost:8088/events?userId=${activeUserId}`).then(resp => 
-            resp.json()
-        );
-    }
-}
+  getEvents(activeUserId) {
+    return fetch(
+      `http://localhost:8088/events?userId=${activeUserId}`
+    ).then(resp => resp.json());
+  },
+  updateEvent(event) {
+    return fetch(`http://localhost:8088/events/${event.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(event)
+    });
+  },
+  saveEvent(event) {
+    return fetch(`http://localhost:8088/events/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(event)
+    });
+  },
+  refillEvent(eventId) {
+    const hiddenEntryId = document.querySelector("#entryId");
+    const nameInput = document.querySelector("#nameInput");
+    const locationInput = document.querySelector("#locationInput");
+    const dateInput = document.querySelector("#dateInput");
+    fetch(`http://localhost:8088/events/${eventId}`)
+      .then(response => response.json())
+      .then(event => {
+        hiddenEntryId.value = event.id;
+        activeUserId = event.userId;
+        nameInput.value = event.name;
+        locationInput.value = event.location;
+        dateInput.value = event.date;
+      });
+  },
+  deleteEvent(eventId) {
+      return fetch(`http://localhost:8088/events/${eventId}`, {
+          method: "DELETE"
+      }).then(response => response.json());
+  }
+};
 
 export default eventAPI;
