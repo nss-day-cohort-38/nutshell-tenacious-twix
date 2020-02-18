aeaa207a381aa9916c30335e958b33873c39524e
Andrew Green <green.andrew.b@gmail.com>
2020-02-18 10:07:01 -0600

    accept

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,17 +2,8 @@ import article from './articles/main.js';
 import kkMainManager from './friends/kkMain.js';
 import tasks from './tasks/main.js'
 
-<<<<<<< HEAD
-sessionStorage.setItem("activeUsers", 1);
-const activeUser = sessionStorage.getItem("activeUsers");
-
-article.runIt();
-tasks.runIt();
-kkMainManager.kkRunIt(activeUser);
-=======
 sessionStorage.setItem(`activeUsers`, 1);
 
 
 article.runIt(sessionStorage.getItem(`activeUsers`));
 kkMainManager.kkRunIt()
->>>>>>> master
