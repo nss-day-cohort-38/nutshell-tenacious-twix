9779faf6a8af830af61333545780ccbee8d217ce
Andrew Green <green.andrew.b@gmail.com>
2020-02-18 09:42:55 -0600

    save

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,16 @@
 import article from "./articles/main.js";
 import kkMainManager from "./friends/kkMain.js";
 import eventsMainManager from "./events/eventsMain.js";
+import {testA, testB, testC} from "./events/eventListeners.js"
 
-sessionStorage.setItem(`activeUsers`, 1);
 
-article.runIt(sessionStorage.getItem(`activeUsers`));
-eventsMainManager.eventNavButton(sessionStorage.getItem(`activeUsers`));
-kkMainManager.kkRunIt(sessionStorage.getItem(`activeUsers`));
+
+testA();
+testB();
+testC();
+
+// sessionStorage.setItem(`activeUsers`, 1);
+
+// article.runIt(sessionStorage.getItem(`activeUsers`));
+// eventsMainManager.eventNavButton(sessionStorage.getItem(`activeUsers`));
+// kkMainManager.kkRunIt(sessionStorage.getItem(`activeUsers`));
