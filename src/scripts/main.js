dd57e6320bb63c969413e5ba8ae4b0b3d38d70eb
Trinity Terry <trinitycterry@gmail.com>
2020-02-18 11:06:52 -0600

    validated session storage

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,15 +10,18 @@ const getURL = () => {
     let url = window.location.href;
     var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
     
-    if(queryString == "home"){
-        article.runIt();
-    } else if (queryString == "friends"){
-        kkMainManager.kkRunIt(activeUser);
-    } else if (queryString == "logout"){
-
-    } else if (queryString == "events"){
-
+    if(activeUser){
+        if(queryString == "home"){
+            article.runIt();
+        } else if (queryString == "friends"){
+            kkMainManager.kkRunIt(activeUser);
+        } else if (queryString == "logout"){
+    
+        } else if (queryString == "events"){
+    
+        }
     }
+
 }
 
  getURL();
