bd1d8bc870f61db7b161e571835d7dfb3739ac09
Trinity Terry <trinitycterry@gmail.com>
2020-02-19 09:52:05 -0600

    deleted console log

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,7 +10,6 @@ const getURL = () => {
   let url = window.location.href;
   var queryString = url ? url.split("?")[1] : window.location.search.slice(1);
 
-  console.log(queryString)
   if (activeUser) {
     if (queryString == "home") {
       article.runIt();
