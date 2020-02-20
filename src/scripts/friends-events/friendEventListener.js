c53512949e3ccab3c656414de7c92a7cf9352a74
Trinity Terry <trinitycterry@gmail.com>
2020-02-20 15:16:41 -0600

    added eventlsitner

--- src/scripts/friends-events/friendEventListener.js
+++ src/scripts/friends-events/friendEventListener.js
@@ -1,32 +1,34 @@
-import renderManager from "../renderManager.js";
-import eventAPI from "./friendEventsAPI.js";
-import htmlManager from "./friendEventsHtmlCreator.js";
-
+import renderManager from '../renderManager.js';
+import eventAPI from './friendEventsAPI.js';
+import htmlManager from './friendEventsHtmlCreator.js';
 let array = [];
-
 const eventsEventListenerManager = {
-  refreshEventContainer: activeUserId => {
-    const container = document.getElementById("container")
-    container.innerHTML = htmlManager.header()
-    eventAPI.getFriends(activeUserId).then(friends => {
-      friends.forEach(friend => {
-        eventAPI.getEvents(friend.userId).then(data => {
-          data.forEach(datum => array.push(datum));
-          array.sort((a, b) => new Date(a.date) - new Date(b.date));
-
-          renderManager.renderFriendEventsToContainer(
-            array,
-            htmlManager.eventsHtmlCreator
-          );
-        });
-      });
-    });
-  },
-
-  signifyNextEvent: () => {
-    let firstEvent = document.getElementById("container").firstElementChild;
-    firstEvent.classList.add("first-event");
-  }
+	refreshEventContainer: activeUserId => {
+		const container = document.getElementById('container');
+		container.innerHTML = htmlManager.header();
+		eventAPI.getFriends(activeUserId).then(friends => {
+			friends.forEach(friend => {
+				eventAPI.getEvents(friend.userId).then(data => {
+					data.forEach(datum => array.push(datum));
+					array.sort((a, b) => new Date(a.date) - new Date(b.date));
+					renderManager.renderFriendEventsToContainer(
+						array,
+						htmlManager.eventsHtmlCreator
+					);
+					if (
+						document.getElementById('friend-events')
+							.childElementCount !== 0
+					) {
+						eventsEventListenerManager.signifyNextEvent();
+					}
+				});
+			});
+		});
+	},
+	signifyNextEvent: () => {
+		let firstEvent = document.getElementById('friend-events')
+			.firstElementChild;
+		firstEvent.classList.add('first-event');
+	}
 };
-
 export default eventsEventListenerManager;
